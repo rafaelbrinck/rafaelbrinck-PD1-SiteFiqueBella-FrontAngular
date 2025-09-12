5ccf86d11f457a928844289fbe892107ae1db530
Rafael Brinckmann <rafabrinckmann@gmail.com>
2025-09-11 22:55:38 -0300

    view: estrutura da lista de clientes (sem funcionalidades)

--- src/app/components/clientes/clientes.ts
+++ src/app/components/clientes/clientes.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from '../../services/cliente-service';
+import { Cliente } from '../../models/cliente';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clientes.html',
-  styleUrl: './clientes.css'
+  styleUrl: './clientes.css',
 })
-export class Clientes {
+export class Clientes implements OnInit {
+  listaClientes: Cliente[] = [];
 
+  constructor(private clienteService: ClienteService) {}
+
+  ngOnInit(): void {
+    this.clienteService.listaClientes$.subscribe((clientes) => {
+      this.listaClientes = clientes;
+    });
+  }
 }
